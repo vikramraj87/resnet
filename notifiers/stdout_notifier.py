d21bac8166efdf1cb8ccb8f4b57aabf6e65c2a55
Vikram Raj <dr.vikramraj87@live.com>
2020-05-11 07:27:14 +0530

    notifiers created

---
+++ notifiers/stdout_notifier.py
@@ -0,0 +1,39 @@
+class StdoutNotifier:
+    def __init__(self,
+                 report_interval=100):
+        self.report_interval = report_interval
+
+        self.running_train_loss = 0
+        self.running_val_loss = 0
+        self.running_val_acc = 0
+
+    def reset(self):
+        self.running_train_loss = 0
+        self.running_val_loss = 0
+        self.running_val_acc = 0
+
+    @staticmethod
+    def epoch_started(epoch):
+        print(f"Epoch: {epoch}")
+
+    def train_step_completed(self, loss):
+        self.running_train_loss += loss
+
+    def validation_step_completed(self, loss, accuracy):
+        self.running_val_loss += loss
+        self.running_val_acc += accuracy
+
+    def step_completed(self, step: int):
+        if step % self.report_interval != 0:
+            return
+
+        train_loss = self.running_train_loss / self.report_interval
+        val_loss = self.running_val_loss / self.report_interval
+        val_acc = self.running_val_acc / self.report_interval
+
+        out = f"[{step:2}] Train loss: {train_loss}; "
+        out += f"Validation loss: {val_loss}; "
+        out += f"Validation accuracy: {val_acc}"
+        print(out)
+
+        self.reset()
---
+++ train/train_runner.py
@@ -0,0 +1,70 @@
+class TrainRunner:
+    def __init__(self,
+                 trainer,
+                 train_loader,
+                 n_epochs,
+                 device,
+                 train_split=0.8,
+                 epochs_completed=0,
+                 steps_completed=0,
+                 notifiers=None):
+        self.trainer = trainer
+        self.train_loader = train_loader
+        self.n_epochs = n_epochs
+        self.device = device
+        self.epochs_completed = epochs_completed
+        self.steps_completed = steps_completed
+        self.notifiers = notifiers
+
+        batch_size = train_loader.batch_size
+        self.n_train = round(train_split * batch_size)
+        self.n_test = batch_size - self.n_train
+
+    def __call__(self):
+        step = self.steps_completed
+        for epoch in range(self.epochs_completed+1, self.n_epochs+1):
+            self._notify("epoch_started",
+                         epoch=epoch)
+
+            for (images, labels) in self.train_loader:
+                step += 1
+                self._notify("step_started",
+                             step=step)
+
+                x = images[:self.n_train].to(self.device)
+                y = labels[:self.n_train].to(self.device)
+
+                loss = self.trainer.train_step(x, y)
+                self._notify("train_step_completed",
+                             loss=loss)
+
+                val_x = images[self.n_train:].to(self.device)
+                val_y = labels[self.n_train:].to(self.device)
+                val_loss, val_acc = self.trainer.validation_step(val_x, val_y)
+                self._notify("validation_step_completed",
+                             loss=val_loss,
+                             accuracy=val_acc)
+                self._notify("step_completed",
+                             step=step)
+
+            model_state_dict = self.trainer.model.state_dict()
+            optim_state_dict = self.trainer.optimizer.state_dict()
+            self._notify("epoch_completed",
+                         epoch=epoch,
+                         step=step,
+                         model_state_dict=model_state_dict,
+                         optim_state_dict=optim_state_dict)
+
+    def _notify(self, action: str, *args, **kwargs):
+        if self.notifiers is None:
+            return
+
+        if not isinstance(self.notifiers, list):
+            notifiers = [self.notifiers]
+        else:
+            notifiers = self.notifiers
+
+        for notifier in notifiers:
+            func = getattr(notifier, action, None)
+            if callable(func):
+                func(*args, **kwargs)

a0054d3413bcf76b9963ddd1885e327fa3d6f5af
Vikram Raj <dr.vikramraj87@live.com>
2020-05-11 23:11:32 +0530

    Minor changes

--- notifiers/comet_notifier.py
+++ notifiers/comet_notifier.py
@@ -14,51 +14,6 @@ class CometNotifier:
         self.val_loss = []
         self.val_acc = []
 
-    def resume_training(self,
-                        step_cache,
-                        train_loss,
-                        val_loss,
-                        val_acc):
-        if len(step_cache) == 0:
-            return
-
-        self.step_cache = []
-        self.train_loss = []
-        self.val_loss = []
-        self.val_acc = []
-
-        first_step = step_cache[0]
-        epoch_str, _ = first_step.split("_")
-        curr_epoch = int(epoch_str)
-        self.experiment.set_epoch(curr_epoch)
-        for step, t_l, v_l, v_a in zip(step_cache,
-                                       train_loss,
-                                       val_loss,
-                                       val_acc):
-            self.step_cache.append(step)
-            epoch_str, step_str = step.split("_")
-            epoch = int(epoch_str)
-            step = int(step_str)
-
-            if epoch > curr_epoch:
-                self.experiment.log_epoch_end(curr_epoch)
-                curr_epoch = epoch
-                self.experiment.set_epoch(curr_epoch)
-
-            self.experiment.set_step(step)
-
-            with self.experiment.train():
-                self.train_loss.append(t_l)
-                self.experiment.log_metric("loss", t_l)
-
-            with self.experiment.validate():
-                self.val_loss.append(v_l)
-                self.val_acc.append(v_a)
-                self.experiment.log_metric("loss", v_l)
-                self.experiment.log_metric("accuracy", v_a)
-
-        epoch_str, step_str = self.step_cache[-1].split("_")
-
     def epoch_started(self, epoch):
         self.experiment.set_epoch(epoch)
 
@@ -116,4 +71,60 @@ class CometNotifier:
         self.experiment.log_asset(fp, file_name=f"{file_name}.pt")
         fp.close()
 
+    @staticmethod
+    def epoch_step(step_str: str) -> (int, int):
+        epoch_str, step_s = step_str.split("_")
+        return int(epoch_str), int(step_s)
+
+    @staticmethod
+    def step_key(epoch: int, step: int) -> str:
+        return f"{epoch}_{step}"
+
+    def resume_state(self, state):
+        self._update(state["steps"],
+                     state["train_loss"],
+                     state["val_loss"],
+                     state["val_accuracy"])
+
+    def _update(self,
+                step_cache,
+                train_loss,
+                val_loss,
+                val_acc):
+        if len(step_cache) == 0:
+            return
+
+        self.step_cache = []
+        self.train_loss = []
+        self.val_loss = []
+        self.val_acc = []
+
+        first_step = step_cache[0]
+        curr_epoch, _ = self.epoch_step(first_step)
+
+        # Update the previous state
+        self.experiment.set_epoch(curr_epoch)
+        for step_str, t_l, v_l, v_a in zip(step_cache,
+                                           train_loss,
+                                           val_loss,
+                                           val_acc):
+            self.step_cache.append(step_str)
+            epoch, step = self.epoch_step(step_str)
+
+            if epoch > curr_epoch:
+                self.experiment.log_epoch_end(curr_epoch)
+                curr_epoch = epoch
+                self.experiment.set_epoch(curr_epoch)
+
+            self.experiment.set_step(step)
+
+            self.train_loss.append(t_l)
+            with self.experiment.train():
+                self.experiment.log_metric("loss", t_l)
+
+            self.val_loss.append(v_l)
+            self.val_acc.append(v_a)
+            with self.experiment.validate():
+                self.experiment.log_metric("loss", v_l)
+                self.experiment.log_metric("accuracy", v_a)
 
